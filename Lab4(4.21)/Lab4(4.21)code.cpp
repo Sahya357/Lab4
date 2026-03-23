08ac07a91a21a842e8209c85d4f4d9e85ce9d177
Sahya357 <97974781+Sahya357@users.noreply.github.com>
2026-03-23 23:25:38 +0200

    Update Lab4(4.21)code.cpp

--- Lab4(4.21)/Lab4(4.21)code.cpp
+++ Lab4(4.21)/Lab4(4.21)code.cpp
@@ -1,19 +1,32 @@
-﻿
 #include <stdio.h>
 #include <locale.h>
 #include <math.h>
+
 int main()
 {
-	setlocale(LC_ALL, "Ukrainian");
-	float x = -2, y = 0;
-	printf("\n********************\n");
-	printf("X\t\tY\n");
-	printf("********************\n");
-	while (x <= 12)
-	{
-		y = (pow(x, 4) - 12 * x) / (x - 2);
-		printf("%f\t%7.2f\n", x, y);
-		x += 0.5;
-	}
-}
+    setlocale(LC_ALL, "Ukrainian");
 
+    float x = -2, y;
+    float b = 12, dx = 0.5;
+
+    printf("\n********************\n");
+    printf("X\t\tY\n");
+    printf("********************\n");
+
+    while (x <= b)
+    {
+        if (fabs(x - 2) < 0.1)
+        {
+            printf("%f\t\tНевизначено (ділення на 0)\n", x);
+        }
+        else
+        {
+            y = (pow(x, 4) - 12 * x) / (x - 2);
+            printf("%f\t%7.2f\n", x, y);
+        }
+
+        x += dx;
+    }
+
+ 
+}
